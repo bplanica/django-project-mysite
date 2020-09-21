f1507328e2ea135d08eeee797aaf0512a3bcff0d
bplanica <bplanica@uw.edu>
2020-09-21 08:17:45 -0500

    assignment 7

--- blogging/models.py
+++ blogging/models.py
@@ -22,3 +22,4 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
--- blogging/views.py
+++ blogging/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 
 from blogging.models import Post
 
+
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
     if args:
@@ -14,11 +15,13 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
+
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'blogging/list.html', context)
+
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
